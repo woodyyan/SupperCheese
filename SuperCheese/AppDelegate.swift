e3245aa8df320804bb657b4bf3851c2c9a755520
mobilenative <mtwang@thoughtworks.com>
2018-01-08 14:46:12 +0800

    Initial Commit

---
+++ SuperCheese/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SuperCheese
+//
+//  Created by Songbai Yan on 08/01/2018.
+//  Copyright © 2018 Songbai Yan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SuperCheese/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SuperCheese
+//
+//  Created by Songbai Yan on 08/01/2018.
+//  Copyright © 2018 Songbai Yan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
