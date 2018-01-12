3a98a43ca409e1a873d779397232580b8e347ca8
mobilenative <mtwang@thoughtworks.com>
2018-01-12 13:22:52 +0800

    Refactor recognize

--- SuperCheese/ViewController.swift
+++ SuperCheese/ViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 
 class ViewController: NSViewController {
     
+    let recognizeEngine = RecognizeEngine()
+    
     var captureController:CaptureWindowController? = nil
     
     var captureViewController:ViewController? = nil
@@ -92,106 +94,11 @@ class ViewController: NSViewController {
             if let videoConnection = stillImageOutput.connection(with: AVMediaType.video){
                 stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (imageBuffer, error) in
                     let imageDataJpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageBuffer!)
-                    self.recognizeImage(imageData: imageDataJpeg)
-                    self.saveImage(imageData: imageDataJpeg)
+                    self.recognizeEngine.recognizeImage(imageData: imageDataJpeg)
+                    //self.saveImage(imageData: imageDataJpeg)
                 })
             }
         }
-    }
-    
-    func recognizeImage(imageData:Data?){
-        if let strBase64 = imageData?.base64EncodedString(options: .lineLength64Characters){
-//            print("IMAGE DATA: \(strBase64)")
-            
-            print("START:")
-            sendRequest(imageBase64String: strBase64)
-        }
-    }
-    
-    func sendRequest(imageBase64String:String){
-        let body = translate(data: imageBase64String)
-        ApiClient_ocr.instance().recoganize(body) { (data, response, error) in
-            print("ERROR:")
-            print(error ?? "")
-            let dataJson = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-            print(dataJson)
-            let dict = dataJson as! [String: Any]
-            let rets = dict["ret"] as! NSArray
-            var sentence = ""
-            for ret in rets{
-                let retDict = ret as! [String: Any]
-                let word = retDict["word"] as! String
-                sentence += word
-                print(word)
-            }
-            self.openBaidu(sentence: sentence)
-        }
-    }
-    
-    func openBaidu(sentence:String){
-        let urlString = "https://www.baidu.com/s"
-        let queryItem = URLQueryItem(name: "wd", value: sentence)
-        let queryItem1 = URLQueryItem(name: "ie", value: "utf-8")
-        let urlComponents = NSURLComponents(string: urlString)!
-        urlComponents.queryItems = [queryItem, queryItem1]
-        if let regURL = urlComponents.url {
-            print(regURL)
-            let result = NSWorkspace.shared.open(regURL)
-            print(result)
-        }
-    }
-    
-    func sendRequestToAliyun(imageBase64String:String){
-        
-        let url = URL(string: "https://tysbgpu.market.alicloudapi.com/api/predict/ocr_general")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.httpBody = translate(data: imageBase64String)
-        request.addValue("application/octet-stream; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        request.addValue("24762824", forHTTPHeaderField: "X-Ca-Key")
-        request.addValue(signature(), forHTTPHeaderField: "X-Ca-Signature")
-        
-        let session = URLSession.shared
-        print("Start task:")
-        let dataTask = session.dataTask(with: request) { (data, response, error) in
-            print("ERROR:")
-            print(error ?? "")
-            print("RESPONSE:")
-            print(response ?? "")
-            print("DATA:")
-            print(data ?? "")
-            if let data = data {
-                if let result = String(data:data,encoding:.utf8){
-                    print(result)
-                }
-            }else {
-                print(error!)
-            }
-        }
-        dataTask.resume()
-    }
-    
-    func signature() -> String{
-        let path = "/api/predict/ocr_general"
-        let url = path
-        
-        let stringToSign = "POST" + "\n" + "" + "\n" + "" + "\n" + "" + "\n" + "" + "\n" + "" + url
-        
-        let sign = stringToSign.hmacsha1(key: "79144b7457ea1be8a9d55fad60848bd7")
-        let signString = sign.base64EncodedString()
-        return signString
-    }
-    
-    func translate(data: String) -> Data {
-        let dict = [
-            "image": data,
-            "configure": [
-                "min_size" : 16,
-                "output_prob" : true
-            ]
-            ] as [String : Any]
-        
-        return try! JSONSerialization.data(withJSONObject: dict, options: [])
     }
     
     func saveImage(imageData:Data?){
